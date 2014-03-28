72fd77b74b356fbfcfebcdef9e95b4ba86593407
aphill93 <austinhill@utexas.edu>
2014-03-28 10:52:11 -0500

    Update binarysearch.m

--- binarysearch.m
+++ binarysearch.m
@@ -1,25 +1,29 @@
-function result = binarysearch(y,T2,P2,r_p)
+function result = binarysearch(y,T2,P2,pNext)
 
 basis = prop_calc(y(1),y(2),y(3),y(4),y(5),P2,T2);
-maxTemp = 2.*T2;
+maxTemp = 4.*T2;
 minTemp = 0;    
-s25s = basis.Smix;
-P25 = P2*r_p;
+sNext = basis.Smix;
+%P25 = P2*r_p;
 %     h25s = basis.h;
 %     %This assumes we know h1 already
 %     h25a = (h25s-h1)/0.9 + h1;
 
 while 1==1
     mTemp = (maxTemp+minTemp)/2;
-    test = prop_calc(y(1),y(2),y(3),y(4),y(5),P25,mTemp);
-    if abs(s25s-test.Smix) < .00000001
+ %  disp(mTemp)
+    test = prop_calc(y(1),y(2),y(3),y(4),y(5),pNext,mTemp);
+ %  disp(s25s)
+ %  disp(test.Smix)
+    if abs(sNext-test.Smix) < .00000001
         break;
     end
-    if s25s > test.Smix
+    if sNext > test.Smix
         minTemp = mTemp;
     end
-    if s25s < test.Smix
+    if sNext < test.Smix
         maxTemp = mTemp;
     end
 end
+%disp(mTemp);
 result = mTemp;
