11748f7bd4b8f8e3aa045dcf2eec51d1622adb01
aphill93 <austinhill@utexas.edu>
2014-03-28 10:52:32 -0500

    Update enthalpy_search.m

--- enthalpy_search.m
+++ enthalpy_search.m
@@ -1,26 +1,29 @@
-function mTemp = enthalpy_search(y,T,P,h_targ)
+ffunction result = binarysearch(y,T2,P2,pNext)
 
-basis = prop_calc(y(1),y(2),y(3),y(4),y(5),P,T);
-maxTemp = 4.*T;
+basis = prop_calc(y(1),y(2),y(3),y(4),y(5),P2,T2);
+maxTemp = 4.*T2;
 minTemp = 0;    
-% h = basis.h;
-% P25 = P2*r_p;
+sNext = basis.Smix;
+%P25 = P2*r_p;
 %     h25s = basis.h;
 %     %This assumes we know h1 already
 %     h25a = (h25s-h1)/0.9 + h1;
 
 while 1==1
     mTemp = (maxTemp+minTemp)/2;
-    test = prop_calc(y(1),y(2),y(3),y(4),y(5),P,mTemp);
-    if abs(h_targ-test.h) < .00000001
+ %  disp(mTemp)
+    test = prop_calc(y(1),y(2),y(3),y(4),y(5),pNext,mTemp);
+ %  disp(s25s)
+ %  disp(test.Smix)
+    if abs(sNext-test.Smix) < .00000001
         break;
     end
-    if h_targ > test.h
+    if sNext > test.Smix
         minTemp = mTemp;
     end
-    if h_targ < test.h
+    if sNext < test.Smix
         maxTemp = mTemp;
     end
 end
-disp(mTemp);
+%disp(mTemp);
 result = mTemp;
